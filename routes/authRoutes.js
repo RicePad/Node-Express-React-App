7feed73e966d6861439fbaab2e32b73ab0adab14
JON <jonn.thinkrtc@gmail.com>
2018-04-01 20:35:26 -0700

    refactor index.js

---
+++ routes/authRoutes.js
@@ -0,0 +1,21 @@
+
+
+	
+const passport = require('passport');
+	
+module.exports = app => {
+  
+  //Route Handlers
+	// app.get('/', (req, res) => {
+	// 	res.send({ hi: 'there' });
+	// });
+
+	  app.get(
+	    '/auth/google',
+	    passport.authenticate('google', {
+	      scope: ['profile', 'email']
+	    })
+	  );
+
+	  app.get('/auth/google/callback', passport.authenticate('google'));
+	};
