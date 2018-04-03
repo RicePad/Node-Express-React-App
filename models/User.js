b1dffb4016ffc965758c99c5a3d80494a57f3128
JON <jonn.thinkrtc@gmail.com>
2018-04-02 21:20:14 -0700

    added googleId property to user schema

--- models/User.js
+++ models/User.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose');
 // const Schema = mongoose.Schema;
 const { Schema } = mongoose;
+
+const userSchema = new Schema({
+	googleId: string
+});
+
+mongoose.model('users', userSchema);
