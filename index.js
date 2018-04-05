e198a718cee3534c8cc8b2065ba0e0e8813ee466
JON <jonn.thinkrtc@gmail.com>
2018-04-04 21:23:53 -0700

    install cookie-session dependancies and set up initial configuration for cookies on index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -9,6 +11,16 @@ require('./services/passport');
 mongoose.connect(keys.mongooseURL);
 
 const app = express();
+
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKeys]
+	})
+	);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
